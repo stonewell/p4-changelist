f118f38884189be8459dc92134a53b43bccc725d
Jingnan Si <jingnan.si@gmail.com>
2020-08-25 23:01:56 -0700

    add shelved file dump for opened

--- dump/__init__.py
+++ dump/__init__.py
@@ -62,21 +62,26 @@ def __dump_changelist(zip_archive):
             logging.info("no file to dump for default change list");
     else:
         if not cfg.arguments.use_shelved:
-            opened_files = __unmarshal_result(p4.run_p4(['describe', cfg.arguments.p4_changelist]).stdout)
+            opened_files = __unmarshal_result(p4.run_p4(['opened', '-c', cfg.arguments.p4_changelist]).stdout)
 
-        # try shelved files, the first object is
-        if cfg.arguments.use_shelved or (not b'depotFile0' in opened_files[0] and b'shelved' in opened_files[0]):
+            if len(opened_files) > 0:
+                __dump_opened_files(zip_archive, root, opened_files)
+            else:
+                logging.info("no file to dump for change list:%s", cfg.arguments.p4_changelist);
+
+        # try shelved files
+        if cfg.arguments.use_shelved or len(opened_files) == 0:
             logging.debug("process shelved files for changelist:%s", cfg.arguments.p4_changelist)
             opened_files = __unmarshal_result(p4.run_p4(['describe',
-                                                         '-du3',
+                                                         '-s',
                                                          '-S',
                                                          cfg.arguments.p4_changelist
                                                          ]).stdout)
 
-        if b'depotFile0' in opened_files[0]:
-            __dump_describe_files(zip_archive, root, opened_files[0])
-        else:
-            logging.info("no file to dump for changelist:%s", cfg.arguments.p4_changelist)
+            if b'depotFile0' in opened_files[0]:
+                __dump_describe_files(zip_archive, root, opened_files[0])
+            else:
+                logging.info("no file to dump for changelist:%s", cfg.arguments.p4_changelist)
 
 def __get_archive_path():
     return os.path.join(cfg.arguments.output_dir,
@@ -158,4 +163,26 @@ def __dump_opened_files_diff(zip_archive, depot_file, client_files):
         z_file.write(bytearray(''.join(contents), 'utf-8'))
 
 def __dump_describe_files(zip_archive, client_root, describe_file):
-    pass
+    file_index = 0
+
+    #find out added files
+    added_files = []
+
+    while True:
+        depot_file = 'depotFile{}'.format(file_index)
+        action = 'action{}'.format(file_index)
+
+        if not (depot_file in describe_file and action in describe_file):
+            break
+
+        added_files.append(describe_file[depot_file])
+        file_index += 1
+
+    # process added files
+    if len(added_files) > 0:
+        for added_file in added_files:
+            __dump_describe_added_file(zip_archive, client_root, added_file)
+
+    # dump edit files
+    diff_content = p4.run_p4(['describe', '-du3', '-S', cfg.arguments.p4_changelist], False).stdout
+    print(diff_content.decode('utf-8'))
